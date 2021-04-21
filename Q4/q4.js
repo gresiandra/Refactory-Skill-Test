d7cd44eb7c33989fa967999d8632ad901f524d0a
gresiandra <gresiandra@gmail.com>
2021-04-22 05:47:04 +0700

    done

--- Q4/q4.js
+++ Q4/q4.js
@@ -1,6 +1,6 @@
 async function getData() {
 
-    const datas = await fetch("./q3.json")
+    const datas = await fetch("./q4.json")
         .then(res => res.json())
         .then(data => data)
         .catch(err => console.log(err))
@@ -10,5 +10,35 @@ async function getData() {
 
 function filteredJson(datas) {
 
-    
+    // Find items in the Meeting Room.
+    const meetingRoomItems = datas.filter(data => {
+        return data.placement.name === "Meeting Room"
+    })
+    for (let i = 0; i < meetingRoomItems.length; i++) {
+        console.log(`${meetingRoomItems[i].name} is in the Meeting Room`)
+    }
+    console.log(" ")
+
+
+    // Find all electronic devices.
+    const electronics = datas.filter(data => {
+        return data.type === "electronic"
+    })
+    for (let i = 0; i < electronics.length; i++) {
+        console.log(`${electronics[i].name} is electronic devices`) 
+    }
+    console.log(" ")
+
+
+    // Find all the furniture
+    const furnitures = datas.filter(data => {
+        return data.type === "furniture"
+    })
+    for (let i = 0; i < furnitures.length; i++) {
+        console.log(`${furnitures[i].name} is furnitures`) 
+    }
+    console.log(" ")
+
 }
+
+getData()
